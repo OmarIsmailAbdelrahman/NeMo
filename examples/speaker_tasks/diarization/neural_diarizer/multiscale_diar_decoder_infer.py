bd541118ee14e3f198de7e23cf9928fd21e75b92
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-14 01:55:55 +0300

    Update multiscale_diar_decoder_infer.py
    Signed-off-by: Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>

--- examples/speaker_tasks/diarization/neural_diarizer/multiscale_diar_decoder_infer.py
+++ examples/speaker_tasks/diarization/neural_diarizer/multiscale_diar_decoder_infer.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import sys
+sys.path.insert(0, '/kaggle/working/NeMo/nemo')  # Replace with the actual path to your cloned NeMo library
 
 from nemo.collections.asr.models.msdd_models import NeuralDiarizer
 from nemo.core.config import hydra_runner
