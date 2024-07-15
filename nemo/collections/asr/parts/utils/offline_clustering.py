628d7c1d8b77420826f4ecd98dcf6cf7299b4247
Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>
2024-07-15 11:36:23 +0300

    Update offline_clustering.py
    Signed-off-by: Omar Ismail <73082049+OmarIsmailAbdelrahman@users.noreply.github.com>

--- nemo/collections/asr/parts/utils/offline_clustering.py
+++ nemo/collections/asr/parts/utils/offline_clustering.py
@@ -673,7 +673,7 @@ def getEnhancedSpeakerCount(
     #comp_est_num_of_spk = torch.tensor(max(torch.mode(torch.tensor(est_num_of_spk_list))[0].item() - anchor_spk_n, 1))
     comp_est_num_of_spk = torch.tensor(max(int(torch.tensor(est_num_of_spk_list).float().mean().item()), 1))                  
     print(f"Legendary-NMESC-getEnhancedSpeakerCount anchor_spk_n {anchor_spk_n} anchor_sample_n {anchor_sample_n} mat.shape {mat.shape} emb.shape {emb.shape} comp_est_num_of_spk {comp_est_num_of_spk}")
-    print(f"torch.tensor(est_num_of_spk_list).float().mean().item() {torch.tensor(est_num_of_spk_list).float().mean().item()}"
+    print(f"torch.tensor(est_num_of_spk_list).float().mean().item() {torch.tensor(est_num_of_spk_list).float().mean().item()}")
     return comp_est_num_of_spk
 
 
